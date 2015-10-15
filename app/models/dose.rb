1960c329a3345ea1582762e5d662cfffc6d88db5
Adrien Liagre <adrienliagre@hotmail.fr>
2015-10-15 12:29:51 +0200

    added validation

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,11 @@
 class Dose < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :cocktail
-  validates :description, uniqueness: true, presence: true
-  validates :cocktail, presence: true, uniqueness: true,
-  validates :ingredient, presence: true, uniqueness: true,
-end
+  # validates :description, presence: true, uniqueness: true
+  # validates :cocktail, presence: true, uniqueness: true
+  validates :ingredient, presence: true, uniqueness: true
+  validates :cocktail, presence: true, :uniqueness => {:scope =>[:cocktail, :ingredient]}
+  end
+
+
+  # is unique for a given cocktail/ingredient couple (FAILED - 3)
